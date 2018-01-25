695e908dd80e31c2c6304a3c5ef1a91494e2e7ec
Warren <warrenoliver1215@gmail.com>
2018-01-25 00:10:36 -0500

    added the empty config file

---
+++ core/config-empty.js
@@ -0,0 +1,12 @@
+// Client ID and API key from the Developer Console
+var API_KEY = 'YOUR_API_KEY';
+var scriptId =
+  'YOUR_SCRIPT_ID';
+var developerKey = 'YOUR_DEV_KEY';
+
+// Replace with your own project number from console.developers.google.com.
+// See "Project number" under "IAM & Admin" > "Settings"
+var appId = "YOUR_APP_ID";
+
+// The Client ID obtained from the Google API Console. Replace with your own Client ID.
+var clientId = "YOUR_CLIENT_ID";
